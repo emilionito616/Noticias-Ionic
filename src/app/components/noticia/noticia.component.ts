1f38583ce1f204ff97df28b1edb3eddfe8511e68
emilionito616 <emilionito9@gmail.com>
2020-02-24 17:10:28 -0600

    Agregando seccion de favoritos

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Article } from '../../interfaces/interfaces';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { ActionSheetController } from '@ionic/angular';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+import { DataLocalService } from '../../services/data-local.service';
 
 @Component({
   selector: 'app-noticia',
@@ -11,7 +14,10 @@ export class NoticiaComponent implements OnInit {
 
   @Input() noticia: Article;
   @Input() indice: number;
-  constructor( private iab: InAppBrowser) { }
+  constructor( private iab: InAppBrowser,
+               private asCtrl: ActionSheetController,
+               private socialsharing: SocialSharing,
+               private dlService: DataLocalService) { }
 
   ngOnInit() {}
 
@@ -20,8 +26,40 @@ export class NoticiaComponent implements OnInit {
     const browser = this.iab.create(this.noticia.url, '_system');
   }
 
-  lanzarMenu() {
-    
+  async lanzarMenu() {
+    const actionSheet = await this.asCtrl.create({
+      buttons: [{
+        text: 'Compartir',
+        icon: 'share',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Share clicked');
+          this.socialsharing.share(
+            this.noticia.title,
+            this.noticia.source.name,
+            '',
+            this.noticia.url
+          );
+        }
+      }, {
+        text: 'Favorito',
+        icon: 'heart',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Favorito');
+          this.dlService.guardarNoticia(this.noticia);
+        }
+      }, {
+        text: 'Cancelar',
+        icon: 'close',
+        cssClass: 'action-dark',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
   }
 
 }
