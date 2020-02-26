cfddf5669d7b4a614b952bddf50d186da1c6699c
emilionito616 <emilionito9@gmail.com>
2020-02-26 12:02:22 -0600

    APK Generada 100%

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiKey: '3e97c47e99d2421a92425d72786c451a',
-  apiUrl: 'http://newsapi.org/v2'
+  apiUrl: 'https://newsapi.org/v2'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiKey: '3e97c47e99d2421a92425d72786c451a',
-  apiUrl: 'http://newsapi.org/v2'
+  apiUrl: 'https://newsapi.org/v2'
 };
 
 /*
